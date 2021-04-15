b4f839e054594b07305b696a7c0ebf473f952c1f
Lynn_Qiu <llynnqiu@gmail.com>
2021-04-14 23:51:29 -0400

    adjust reducers

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 
 import entities from "./entities_reducer";
 import ui from "./ui_reducer";
-import errors from "./errors_reducer"
+import errors from "./session_errors_reducer";
 import session from "./session_reducer";
 
 const RootReducer = combineReducers({
